307457e297e50ffe1fc74edba931b7dd963c8b66
Ambre Lala <ambre.lala@penninghen.fr>
2022-12-19 15:03:46 +0100

    controllers

--- app/controllers/lists_controller.rb
+++ app/controllers/lists_controller.rb
@@ -1,2 +1,40 @@
 class ListsController < ApplicationController
+
+
+  def index
+    @lists = List.all
+  end
+
+  def show
+    @bookmark = Bookmark.new
+    # @review = Review.new(list: @list)
+  end
+
+  def new
+    @lists = List.new
+  end
+
+  def create
+    @lists = List.new(list_params)
+    if @list.save
+      redirect_to list_path(@list)
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  def destroy
+    @list.destroy
+    redirect_to list_path, status: :see_other
+  end
+
+  private
+
+  def set_list
+    @list = List.find(params[:id])
+  end
+
+  def list_params
+    params.require(:list).permit(:name, :photo)
+  end
 end
